e55ce936fd2d7b20b8dd75a0adcdc30544a7b3da
Halfis_Mutaqin <halfismutaqin@gmail.com>
2024-01-19 10:30:37 +0700

    Init

---
+++ myBackendApi/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+use App\Models\Passport\AuthCode;
+use App\Models\Passport\Client;
+use App\Models\Passport\PersonalAccessClient;
+use App\Models\Passport\RefreshToken;
+use App\Models\Passport\Token;
+
+// use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+        // Passport::routes(); // <-- passport route
+
+        //
+    }
+}
---
+++ myFrontend/src/views/home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h3 class="display-5 fw-bold">VUE 3 (VITE) + LARAVEL 10</h3>
+            <p class="col-md-8 fs-4">Simple Project CRUD &copy; @Halfis</p>
+        </div>
+    </div>
+</template>
